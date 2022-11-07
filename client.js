581157813d3aafa49c131e9a3413f2b3e6f89dff
Dominik Raup <dominik.raup@student.fh-kiel.de>
2022-11-08 00:08:50 +0100

    synced

--- client.js
+++ client.js
@@ -4,6 +4,109 @@
 import * as alt from 'alt-client';
 import * as native from 'natives';
 
+alt.on("gameEntityCreate", (entity) => {
+    if(entity.hasStreamSyncedMeta("isTowingPl")) {
+        let player = entity;
+        let veh = entity.getStreamSyncedMeta("veh");
+
+        let rope = native.addRope(player.pos.x, player.pos.y, player.pos.z, 0, 0, 0, 5, 3, 5, 1, 1, false, false, false, 5, false, null);
+
+        let vehBone = native.getWorldPositionOfEntityBone(veh, native.getEntityBoneIndexByName(veh, "brakelight_l"));
+
+        native.activatePhysics(rope);
+        native.ropeLoadTextures();
+        native.attachEntitiesToRope(rope, player, veh, player.pos.x, player.pos.y, player.pos.z, vehBone.x, vehBone.y, vehBone.z, 5, false, false, null, null);
+
+        alt.LocalStorage.set("playerRope", rope);
+    }
+    if(entity.hasStreamSyncedMeta("isTowing")) {
+        let veh2 = entity;
+        let veh1 = entity.getStreamSyncedMeta("veh");
+
+        let rope = native.addRope(veh2.pos.x, veh2.pos.y, veh2.pos.z, 0, 0, 0, 5, 3, 5, 1, 1, false, false, false, 5, false, null);
+
+        let vehBone = native.getWorldPositionOfEntityBone(veh1, native.getEntityBoneIndexByName(veh1, "headlight_l"));
+
+        let vehBone2 = native.getWorldPositionOfEntityBone(veh2, native.getEntityBoneIndexByName(veh2, "brakelight_l"));
+
+        native.activatePhysics(rope);
+        native.ropeLoadTextures();
+        native.attachEntitiesToRope(rope, veh1, veh2, vehBone.x, vehBone.y, vehBone.z, vehBone2.x, vehBone2.y, vehBone2.z, 5, false, false, null, null);
+
+        alt.LocalStorage.set(veh2, rope);
+
+    }
+});
+
+alt.on("streamSyncedMetaChange", (entity, key, newValue, oldValue) => {
+    
+    if(key === "isTowingPl") {
+        let player = entity;
+        if(newValue) {
+            let veh = entity.getStreamSyncedMeta("veh");
+
+            const textEntry = `TEXT_ENTRY_${(Math.random() * 1000).toFixed(0)}`;
+             alt.addGxtText(textEntry, veh);
+  
+                native.beginTextCommandThefeedPost('STRING');
+             native.addTextComponentSubstringTextLabel(textEntry);
+            native.endTextCommandThefeedPostTicker(false, false);
+
+            let rope = native.addRope(player.pos.x, player.pos.y, player.pos.z, 0, 0, 0, 5, 3, 5, 1, 1, false, false, false, 5, false, null);
+
+            let vehBone = native.getWorldPositionOfEntityBone(veh, native.getEntityBoneIndexByName(veh, "brakelight_l"));
+
+            native.activatePhysics(rope);
+            native.ropeLoadTextures();
+            native.attachEntitiesToRope(rope, player, veh, player.pos.x, player.pos.y, player.pos.z, vehBone.x, vehBone.y, vehBone.z, 5, false, false, null, null);
+    
+            alt.LocalStorage.set("playerRope", rope);
+        }
+        else
+        {
+            let ropeId = alt.LocalStorage.get("playerRope");
+            let veh = player.getStreamSyncedMeta("veh");
+            native.detachRopeFromEntity(ropeId, player);
+            native.detachRopeFromEntity(ropeId, veh);
+            native.ropeUnloadTextures(ropeId);
+            native.deleteRope(ropeId);
+            alt.LocalStorage.delete("playerRope");
+        }
+        
+    }
+    if(key === "isTowing") {
+        if(newValue)
+        {
+            let veh2 = entity;
+            let veh1 = entity.getStreamSyncedMeta("veh");
+            let rope = native.addRope(veh2.pos.x, veh2.pos.y, veh2.pos.z, 0, 0, 0, 5, 3, 5, 1, 1, false, false, false, 5, false, null);
+    
+            let vehBone = native.getWorldPositionOfEntityBone(veh1, native.getEntityBoneIndexByName(veh1, "headlight_l"));
+    
+            let vehBone2 = native.getWorldPositionOfEntityBone(veh2, native.getEntityBoneIndexByName(veh2, "brakelight_l"));
+    
+            native.activatePhysics(rope);
+            native.ropeLoadTextures();
+            native.attachEntitiesToRope(rope, veh1, veh2, vehBone.x, vehBone.y, vehBone.z, vehBone2.x, vehBone2.y, vehBone2.z, 5, false, false, null, null);
+
+            alt.LocalStorage.set(veh2, rope);
+        }
+        else if(newValue!=oldValue)
+        {
+            let veh2 = entity;
+            let veh1 = entity.getStreamSyncedMeta("veh");
+            let rope = alt.LocalStorage.get(veh2);
+            native.detachRopeFromEntity(rope, veh1);
+            native.detachRopeFromEntity(rope, veh2);
+            native.ropeUnloadTextures(rope);
+            native.deleteRope(rope);
+            alt.LocalStorage.delete(veh2);
+        }
+        
+    }
+});
+
+
 alt.onServer("attachRope", (player, vehicle) => {
     let veh = vehicle;
 
@@ -16,6 +119,7 @@ alt.onServer("attachRope", (player, vehicle) => {
     native.attachEntitiesToRope(rope, player, vehicle, player.pos.x, player.pos.y, player.pos.z, vehBone.x, vehBone.y, vehBone.z, 5, false, false, null, null);
 
     const data = {
+                 "Player": player,
                  "Rope": rope,
                  "Veh": vehicle
     };
@@ -35,6 +139,7 @@ alt.onServer("attachRopeCar", (player, veh1, veh2, ropeId) => {
     native.setVehicleBrake(veh1, false);
 
     const data = {
+                 "Player": player,
                  "Rope": ropeId,
                  "Veh1": veh1,
                  "Veh2": veh2
@@ -49,8 +154,13 @@ alt.onServer("untowPlayer", (player, ropeId, veh) => {
     native.detachRopeFromEntity(ropeId, veh);
     native.ropeUnloadTextures(ropeId);
     native.deleteRope(ropeId);
+
+    const data = {
+        "Player": player,
+        "Veh": veh
+    }
     
-    alt.emitServer("untowPlayer", veh);
+    alt.emitServer("untowPlayer", data);
 })
 
 alt.onServer("untowCar", (player, veh1, veh2, rope) => {
@@ -61,6 +171,7 @@ alt.onServer("untowCar", (player, veh1, veh2, rope) => {
     native.deleteRope(rope);
 
     const data = {
+        "Player": player,
         "Veh1": veh1,
         "Veh2": veh2
     }
